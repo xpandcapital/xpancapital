3770ed2b696d0a140905c6483041f7dff0d6be40
kvdlc <kvdlc@users.noreply.github.com>
2026-05-01 14:28:13 -0500

    fix: add ProjectContext to hooks barrel export

--- app/superadmin/gestion-lotes/[slug]/_hooks/index.ts
+++ app/superadmin/gestion-lotes/[slug]/_hooks/index.ts
@@ -3,3 +3,4 @@ export { useLotes } from './useLotes';
 export { useProjectConfig } from './useProjectConfig';
 export { useAIProcessing } from './useAIProcessing';
 export { useSorteo } from './useSorteo';
+export { useProject, ProjectProvider } from './ProjectContext';
