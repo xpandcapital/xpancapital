4eb4973f2ed876f063b4c588757608bf9095d506
kvdlc <kvdlc@users.noreply.github.com>
2026-06-15 16:47:18 -0500

    Fix: margen izquierdo del contenido respeta ancho real del sidebar (56px móvil=pl-14, 64px desktop=pl-16)

--- app/superadmin/layout.tsx
+++ app/superadmin/layout.tsx
@@ -12,7 +12,7 @@ export default function AdminLayout({
     return (
         <div className="flex bg-black min-h-[calc(100dvh-80px)] overflow-x-hidden">
             <SuperadminSidebar />
-            <div className="flex-1 pt-20 md:pt-24 pl-4 md:pl-6 lg:pl-16">
+            <div className="flex-1 pt-20 md:pt-24 pl-14 lg:pl-16">
                 <div className="max-w-[1600px] mx-auto w-full pb-20 overflow-x-hidden">
                     <SuperadminGuard>
                         <GlobalSearch />
